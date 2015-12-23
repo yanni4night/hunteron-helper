0c2137bff27c128aacb093691c04b7952f6dae9b
yinyong <yanni4night@gmail.com>
2015-12-24 01:02:35 +0800

    semantic supported

--- scripts/popup.js
+++ scripts/popup.js
@@ -10,7 +10,11 @@
  * @since 0.1.0
  */
 
-var pageSize = 100;
+var DEBUG = true;
+
+var pageSize = DEBUG ? 15 : 100;
+
+
 
 function getQueryUrl(start) {
     return 'http://hd.hunteron.com/api/v1/position/query?_t=' + Date.now() + '&cityId=30101&industryId=101&size=' +
@@ -20,43 +24,57 @@ function getQueryUrl(start) {
 function queryJobs(cb) {
     var jobList = [];
 
-    $.getJSON(getQueryUrl(0)).done(function (ret) {
+    $.getJSON(getQueryUrl(0)).done(function(ret) {
+
+        if (!ret || !ret.success) {
+            return cb(new Error(ret && ret.message || '未知错误'));
+        }
+
         var total = ret.data.total;
-        var remainQueries = Math.ceil(total / pageSize) - 1;
+        var remainQueries = DEBUG ? 0 : Math.ceil(total / pageSize) - 1;
+
+
 
         if (ret.data && Array.isArray(ret.data.positions)) {
             jobList = jobList.concat(ret.data.positions);
         }
+
+        /*if (debug) {
+            return cb(null, jobList);
+        }*/
 
         var queries = [];
 
         for (var i = 0; i < remainQueries; ++i) {
             queries.push($.getJSON(getQueryUrl((1 + i) * pageSize)));
         }
-        $.when.apply($, queries).done(function () {
-            Array.prototype.map.call(arguments, function (rets) {
+
+        $.when.apply($, queries).done(function() {
+            Array.prototype.map.call(arguments, function(rets) {
                 var ret = rets[0];
                 if (ret.data && Array.isArray(ret.data.positions)) {
                     jobList = jobList.concat(ret.data.positions);
                 }
             });
-            var finalJobs = jobList.filter(function (pos) {
+            var finalJobs = jobList.filter(function(pos) {
                 return pos.positionType != 0 || pos.applyStatus === 1;
+            }).map(function(item) {
+                return item.positionId;
             });
 
-            cb(finalJobs);
+            cb(null, finalJobs);
         });
 
-    }).fail(function () {
-        console.log('fail');
+    }).fail(function() {
+        cb(new Error('fail'));
     });
 }
 
 function queryJobDetail(jobIds) {
-    var promises = jobIds.map(function (id) {
-        return new Promise(function (resolve) {
+    var promises = jobIds.map(function(id) {
+        return new Promise(function(resolve) {
             $.getJSON('http://hd.hunteron.com/api/v1/position/detail?_t=' + Date.now() + '&positionId=' +
-                id).done(function (data) {
+                id).done(function(data) {
                 resolve(data);
             });
         });
@@ -65,25 +83,30 @@ function queryJobDetail(jobIds) {
     return Promise.all(promises);
 }
 
-$('button').click(function () {
-    new Promise(function (resolve) {
-        queryJobs(function (jobs) {
-            resolve(jobs.map(function (item) {
-                return item.positionId;
-            }).slice(0, 5));
+function showError(e) {
+    $('.ui.basic.modal').find('.header').text(e.message).end().modal('show');
+
+}
+
+$('button').click(function() {
+    new Promise(function(resolve, reject) {
+        queryJobs(function(err, jobIds) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(jobIds);
+            }
         });
-    }).then(function (jobIds) {
+    }).then(function(jobIds) {
         return queryJobDetail(jobIds);
-    }).then(function (data) {
-        var positions = data.map(function (item) {
+    }).then(function(data) {
+        var positions = data.map(function(item) {
             return item.data.position;
-        }).forEach(function (item, idx) {
-            $('<tr><td>' + (idx + 1) + '</td><td>' + item.positionTitle + '</td></tr>').appendTo(
+        }).forEach(function(item, idx) {
+            $('<tr><td>' + (idx + 1) + '</td><td>' + item.positionTitle + '</td><td>'+item.jobDescription+'</td><td>'+item.jobRequirement+'</td></tr>').appendTo(
                 $('tbody'));
         });
 
-    }).catch(function (e) {
-        alert(e.message);
-    });
+    }).catch(showError);
 
 });
